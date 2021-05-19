10a1054ca50c4fccd91fd300053af3da236bb40f
tya-aretya <84399446+tya-aretya@users.noreply.github.com>
2021-05-19 12:21:03 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u tya.aretya@gmail.com.bang_worker -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RFpob5XqzgpohoCezvgCo8heaXBaT5DZek.bang_worker -p x --cpu 4
